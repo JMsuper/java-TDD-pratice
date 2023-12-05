305c30d9fc2dfbfb2fe55adf1580a34d12844a1a
JMsuper <tjsqls2067@naver.com>
2023-12-05 11:36:16 +0900

    apply value object pattern

--- tdd/src/main/java/org/example/chapter1/Dollar.java
+++ tdd/src/main/java/org/example/chapter1/Dollar.java
@@ -5,7 +5,15 @@ public class Dollar {
     public Dollar(int amount){
         this.amount = amount;
     }
+
     public Dollar times(int multiplier){
         return new Dollar(this.amount * multiplier);
+    }
+
+    @Override
+    public boolean equals(Object object){
+        Dollar d = (Dollar) object;
+        return this.amount == d.amount;
+
     }
 }
