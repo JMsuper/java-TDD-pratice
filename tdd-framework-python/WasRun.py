8d6e1c9f3744906a1a00b9217b45ccc24bd33668
JMsuper <tjsqls2067@naver.com>
2023-12-08 11:39:14 +0900

    add tearDown method & convert flag to log

--- tdd-framework-python/WasRun.py
+++ tdd-framework-python/WasRun.py
@@ -5,38 +5,37 @@ class TestCase:
     def setUp(self):
         pass
     
+    def tearDown(self):
+        pass
+    
     def run(self):
         self.setUp()
         method = getattr(self, self.name)
         method()
+        self.tearDown()
 
 
 class WasRun(TestCase):
     def __init__(self,name):
-        self.wasSetUp = None
         TestCase.__init__(self,name)
 
     def setUp(self):
-        self.wasRun = None
-        self.wasSetUp = 1
+        self.log = "setUp "
     
     def testMethod(self):
-        self.wasRun = 1
+        self.log = self.log + "testMethod "
+
+    def tearDown(self):
+        self.log = self.log + "tearDown "
 
 
 class TestCaseTest(TestCase):
-    def setUp(self):
-        self.test = WasRun("testMethod")
-
-    def testRunning(self):
-        self.test.run()
-        assert(self.test.wasRun)
-
-    def testSetUp(self):
-        self.test.run()
-        assert(self.test.wasSetUp)
+    def testTemplateMethod(self):
+        test = WasRun("testMethod")
+        test.run()
+        assert("setUp testMethod tearDown " == test.log)
 
 
 # 부트스트랩 테스터(테스트 프레임워크를 테스트하는 테스터)
 if __name__ == "__main__":
-    TestCaseTest("testSetUp").run()
+    TestCaseTest("testTemplateMethod").run()
