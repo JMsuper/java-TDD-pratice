488fdf2c4fa3216e3a749f16886e81a1df5c1ff9
JMsuper <tjsqls2067@naver.com>
2023-12-08 11:20:43 +0900

    add setUp method

--- tdd-framework-python/WasRun.py
+++ tdd-framework-python/WasRun.py
@@ -1,16 +1,42 @@
-class WasRun:
+class TestCase:
     def __init__(self,name):
-        self.wasRun = None
+        self.name = name
 
+    def setUp(self):
+        pass
+    
     def run(self):
-        self.testMethod()
+        self.setUp()
+        method = getattr(self, self.name)
+        method()
+
+
+class WasRun(TestCase):
+    def __init__(self,name):
+        self.wasSetUp = None
+        TestCase.__init__(self,name)
+
+    def setUp(self):
+        self.wasRun = None
+        self.wasSetUp = 1
     
     def testMethod(self):
         self.wasRun = 1
 
+
+class TestCaseTest(TestCase):
+    def setUp(self):
+        self.test = WasRun("testMethod")
+
+    def testRunning(self):
+        self.test.run()
+        assert(self.test.wasRun)
+
+    def testSetUp(self):
+        self.test.run()
+        assert(self.test.wasSetUp)
+
+
 # 부트스트랩 테스터(테스트 프레임워크를 테스트하는 테스터)
 if __name__ == "__main__":
-    test = WasRun("testMethod")
-    print(test.wasRun)
-    test.run()
-    print(test.wasRun)
+    TestCaseTest("testSetUp").run()
